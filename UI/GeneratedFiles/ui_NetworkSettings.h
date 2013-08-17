9775814c5557aec937cc1a42e3a7abfbd7e530fe
hnefatl <hnefatl@gmail.com>
2013-08-17 17:27:49 +0100

    Working

--- UI/GeneratedFiles/ui_NetworkSettings.h
+++ UI/GeneratedFiles/ui_NetworkSettings.h
@@ -21,7 +21,7 @@
 
 QT_BEGIN_NAMESPACE
 
-class Ui_Form_NetworkSettings
+class Ui_Dialog_NetworkSettings
 {
 public:
     QLabel *label;
@@ -32,52 +32,52 @@ public:
     QLineEdit *Text_ServerPort;
     QPushButton *Button_Connect;
 
-    void setupUi(QWidget *Form_NetworkSettings)
+    void setupUi(QWidget *Dialog_NetworkSettings)
     {
-        if (Form_NetworkSettings->objectName().isEmpty())
-            Form_NetworkSettings->setObjectName(QStringLiteral("Form_NetworkSettings"));
-        Form_NetworkSettings->resize(250, 150);
-        label = new QLabel(Form_NetworkSettings);
+        if (Dialog_NetworkSettings->objectName().isEmpty())
+            Dialog_NetworkSettings->setObjectName(QStringLiteral("Dialog_NetworkSettings"));
+        Dialog_NetworkSettings->resize(250, 150);
+        label = new QLabel(Dialog_NetworkSettings);
         label->setObjectName(QStringLiteral("label"));
         label->setGeometry(QRect(10, 10, 111, 16));
-        Text_Username = new QLineEdit(Form_NetworkSettings);
+        Text_Username = new QLineEdit(Dialog_NetworkSettings);
         Text_Username->setObjectName(QStringLiteral("Text_Username"));
         Text_Username->setGeometry(QRect(120, 10, 113, 20));
         Text_Username->setMaxLength(25);
-        label_2 = new QLabel(Form_NetworkSettings);
+        label_2 = new QLabel(Dialog_NetworkSettings);
         label_2->setObjectName(QStringLiteral("label_2"));
         label_2->setGeometry(QRect(10, 40, 111, 16));
-        Text_ServerIP = new QLineEdit(Form_NetworkSettings);
+        Text_ServerIP = new QLineEdit(Dialog_NetworkSettings);
         Text_ServerIP->setObjectName(QStringLiteral("Text_ServerIP"));
         Text_ServerIP->setGeometry(QRect(120, 40, 113, 20));
-        label_3 = new QLabel(Form_NetworkSettings);
+        label_3 = new QLabel(Dialog_NetworkSettings);
         label_3->setObjectName(QStringLiteral("label_3"));
         label_3->setGeometry(QRect(10, 70, 111, 16));
-        Text_ServerPort = new QLineEdit(Form_NetworkSettings);
+        Text_ServerPort = new QLineEdit(Dialog_NetworkSettings);
         Text_ServerPort->setObjectName(QStringLiteral("Text_ServerPort"));
         Text_ServerPort->setGeometry(QRect(120, 70, 113, 20));
-        Button_Connect = new QPushButton(Form_NetworkSettings);
+        Button_Connect = new QPushButton(Dialog_NetworkSettings);
         Button_Connect->setObjectName(QStringLiteral("Button_Connect"));
         Button_Connect->setGeometry(QRect(90, 110, 75, 23));
 
-        retranslateUi(Form_NetworkSettings);
+        retranslateUi(Dialog_NetworkSettings);
 
-        QMetaObject::connectSlotsByName(Form_NetworkSettings);
+        QMetaObject::connectSlotsByName(Dialog_NetworkSettings);
     } // setupUi
 
-    void retranslateUi(QWidget *Form_NetworkSettings)
+    void retranslateUi(QWidget *Dialog_NetworkSettings)
     {
-        Form_NetworkSettings->setWindowTitle(QApplication::translate("Form_NetworkSettings", "Network Settings", 0));
-        label->setText(QApplication::translate("Form_NetworkSettings", "Enter your username: ", 0));
-        label_2->setText(QApplication::translate("Form_NetworkSettings", "Enter the server IP: ", 0));
-        label_3->setText(QApplication::translate("Form_NetworkSettings", "Enter the server port: ", 0));
-        Button_Connect->setText(QApplication::translate("Form_NetworkSettings", "Connect", 0));
+        Dialog_NetworkSettings->setWindowTitle(QApplication::translate("Dialog_NetworkSettings", "Network Settings", 0));
+        label->setText(QApplication::translate("Dialog_NetworkSettings", "Enter your username: ", 0));
+        label_2->setText(QApplication::translate("Dialog_NetworkSettings", "Enter the server IP: ", 0));
+        label_3->setText(QApplication::translate("Dialog_NetworkSettings", "Enter the server port: ", 0));
+        Button_Connect->setText(QApplication::translate("Dialog_NetworkSettings", "Connect", 0));
     } // retranslateUi
 
 };
 
 namespace Ui {
-    class Form_NetworkSettings: public Ui_Form_NetworkSettings {};
+    class Dialog_NetworkSettings: public Ui_Dialog_NetworkSettings {};
 } // namespace Ui
 
 QT_END_NAMESPACE
---
+++ UI/NetworkSettings.h
@@ -0,0 +1,19 @@
+#ifndef _NETWORKSETTINGS_H
+#define _NETWORKSETTINGS_H
+
+#include <QtWidgets\qwidget.h>
+#include "GeneratedFiles\ui_NetworkSettings.h"
+
+class NetworkSettings : public QWidget
+{
+	Q_OBJECT
+
+public:
+	NetworkSettings(QWidget *Parent=NULL);
+	~NetworkSettings();
+
+private:
+	Ui::Dialog_NetworkSettings ui;
+};
+
+#endif
